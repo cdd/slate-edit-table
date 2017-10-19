67126dd15b53204feeda4d615a1d84d573d008a2
Kelly Joseph Price <kelly@collaborativedrug.com>
2017-10-19 11:42:58 -0700

    Allow inserting of arbitrary blocks

---
+++ tests/insert-row-with-cell-getter/change.js
@@ -0,0 +1,39 @@
+import Slate from 'slate';
+
+const contents = [
+    "Col 0, Row 2",
+    Slate.Block.create({
+      type: "paragraph",
+      nodes: [
+        Slate.Text.create({
+          text: "Col 1, Row 2"
+        })
+      ]
+    }),
+    [
+        Slate.Block.create({
+            type: "paragraph",
+            nodes: [
+                Slate.Text.create({
+                    text: "Col 2,"
+                })
+            ]
+        }),
+        Slate.Block.create({
+            type: "paragraph",
+            nodes: [
+                Slate.Text.create({
+                    text: " Row 2"
+                })
+            ]
+        })
+    ]
+];
+
+module.exports = function(plugin, change) {
+    const { state } = change;
+    const cursorBlock = state.document.getDescendant('_cursor_');
+    change.moveToRangeOf(cursorBlock);
+
+    return plugin.changes.insertRow(change, 2, i => contents[i]);
+};
